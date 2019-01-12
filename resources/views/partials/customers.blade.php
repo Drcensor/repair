0e35a205a3ff8df1f24d0f2eaa60fd3379eb7247
Tyrone Williams <drcensor@gmail.com>
2019-01-12 22:10:09 +0000

    init laravel

---
+++ resources/views/partials/customers.blade.php
@@ -0,0 +1,11 @@
+<p>Print icon on a styled link button:
+  <a href="#" class="btn btn-success btn-lg">
+    <span class="glyphicon glyphicon-print"></span> Print
+  </a>
+</p>
+
+<p>Plus-sign icon on a styled link button:
+  <a href="#" class="btn btn-info btn-lg">
+    <span class="glyphicon glyphicon-plus-sign"></span> Plus
+  </a>
+</p>
---
+++ resources/views/partials/search.blade.php
@@ -0,0 +1,10 @@
+
+
+      <p>  <input type="text" name="theName" value="Customer Surname"
+  onblur="if(this.value==''){ this.value='Customer surname'; this.style.color='#000';}"
+  onfocus="if(this.value=='Customer Surname'){ this.value=''; this.style.color='#000';}"
+  style="color:#000;" />
+        <button type="button" class="btn btn-info">
+          <span class="glyphicon glyphicon-search"></span> Search
+        </button>
+      </p>
